7b1fceb3354569253335a851f8ae163c9290c7c8
Tanmay Nikhare <tanmaynikhare200@gmail.com>
2025-08-14 00:01:29 +0530

    Update test_email.py

--- updated_tunewithin/test_email.py
+++ updated_tunewithin/test_email.py
@@ -1,10 +1,11 @@
 from utils import send_email_report
 
 # Replace with your own email for testing
-test_email = "nirmayee.ninawe24@vit.edu"
+test_email = "tanmaynikhare@gmail.com"
 
 send_email_report(
     to_email=test_email,
     subject="🧪 Test Email from TuneWithin",
     body="This is a test email to verify that the email-sending function is working!"
 )
+
